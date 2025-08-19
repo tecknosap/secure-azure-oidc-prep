10d553c1840f00d4b1c7f08c81340e3c414a5584
Mahad Mohamed <tecknosap@gmail.com>
2025-08-19 22:35:30 +0100

    initail commit

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "prep" {
+  
+name = var.rg_name
+location = var.location
+}
+
+
+
+resource "azurerm_virtual_network" "vnet1" {
+  name = var.vnet_name
+  resource_group_name = azurerm_resource_group.prep.name
+  location = azurerm_resource_group.prep.location
+  address_space = [var.vnet_address_space]
+}
+resource "azurerm_subnet" "subnet1" {
+  name = var.subnet1_name
+  resource_group_name = azurerm_resource_group.prep.name
+  virtual_network_name = azurerm_virtual_network.vnet1.id
+  address_prefixes = [var.subnet1_prefix]
+  
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name = var.subnet1_name
+  resource_group_name = azurerm_resource_group.prep.name
+  virtual_network_name = azurerm_virtual_network.vnet1.id
+  address_prefixes = [var.subnet2_prefix]
+  
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,51 @@
+# Resource Group
+variable "rg_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "myResourceGroup"
+}
+
+# Location
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "eastus"
+}
+
+# Virtual Network
+variable "vnet_name" {
+  description = "Name of the Virtual Network"
+  type        = string
+  default     = "myVNet"
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the Virtual Network"
+  type        = string
+  default = ["10.0.0.0/16"]
+}
+
+# Subnets
+variable "subnet1_name" {
+  description = "Name of the first subnet"
+  type        = string
+  default     = "subnet1"
+}
+
+variable "subnet1_prefix" {
+  description = "Address prefix for the first subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet2_name" {
+  description = "Name of the second subnet"
+  type        = string
+  default     = "subnet2"
+}
+
+variable "subnet2_prefix" {
+  description = "Address prefix for the second subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
