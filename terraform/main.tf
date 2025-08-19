0bc16d9aac0b088f68919b21ab5dd3bee6fb53f7
Mahad Mohamed <tecknosap@gmail.com>
2025-08-19 22:57:37 +0100

    initail commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_subnet" "subnet1" {
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name = var.subnet1_name
+  name = var.subnet2_name
   resource_group_name = azurerm_resource_group.prep.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes = [var.subnet2_prefix]
